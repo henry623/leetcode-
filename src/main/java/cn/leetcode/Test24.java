592dc88b6f8a2b172a94d56ac6e4cba78b9e940f
henry623 <1165882708@qq.com>
2025-04-09 01:15:48 +0800

    Leetcode算法 24 25

--- src/main/java/cn/leetcode/Test24.java
+++ src/main/java/cn/leetcode/Test24.java
@@ -18,4 +18,50 @@ public class Test24 {
      * 链表中节点的数目在范围 [0, 100] 内
      * 0 <= Node.val <= 100
      */
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode listNode = new Test24().swapPairs(node1);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+
+
+
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    public ListNode swapPairs2(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+
 }
